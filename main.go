d00a5553bef3d486b67acb10c6f949959220bd61
Patrick Handberg Jessen <patj@itu.dk>
2023-09-20 11:50:39 +0200

    fun

--- main.go
+++ main.go
@@ -1,16 +1,94 @@
 package main
 
-type pack struct {
+import (
+	"fmt"
+	"sync"
+)
+
+var wait sync.WaitGroup
+
+var channel = make(chan *TCP)
+
+type TCP struct {
+	src int
+	dst int
+	seq int
+	ack int
+
+	SYN bool
+	RST bool
+	ACK bool
+	FIN bool
+
+	data string
 }
 
-func client() {
+func newPackage(seq int, ack int) *TCP {
+	p := TCP{}
+	p.seq = seq
+	p.ack = ack
+	return &p
+}
+
+func client(ip int) {
+	defer wait.Done()
+
+	seq := randomNumber()
+	channel <- newPackage(seq, 0)
+
+	p := <-channel
+
+	if seq+1 != p.ack {
+		fmt.Println("Connection failed!")
+		return
+	}
+
+	channel <- newPackage(p.ack, p.seq+1)
+
+	for {
+
+	}
+}
+
+func server(ip int) {
+	defer wait.Done()
+
+	seq := randomNumber()
+	p := <-channel
+	channel <- newPackage(seq, p.seq+1)
+
+	p = <-channel
+
+	if seq+1 != p.ack {
+		fmt.Println("Connection failed!")
+		return
+	}
+
+	for {
+
+	}
+}
+
+func listen() {
 
 }
 
-func server() {
+func connect() {
+
+}
+
+func close() {
+
+}
+
+func send() {
 
 }
 
 func main() {
+	go client(1)
+	go server(2)
+	wait.Add(1)
 
+	wait.Wait()
 }
